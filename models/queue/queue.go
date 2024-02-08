315ee2d8fbd8a59fb78705af9ace5aaa98b5c208
Pedram Sadeghian <pedy4000@gmail.com>
2024-02-09 03:07:52 +0330

    debug

--- models/queue/queue.go
+++ models/queue/queue.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"encoding/json"
+	"fmt"
 	"math/rand"
 
 	"github.com/gorilla/websocket"
@@ -23,6 +24,12 @@ type Queue struct {
 }
 
 func (q *Queue) Push(data Data) error {
+	fmt.Println("TTTTTTTTTT")
+	fmt.Println(data)
+	fmt.Println(q)
+	fmt.Println(q.KeySet)
+	fmt.Println(q.KeySet[data.Key])
+
 	if _, ok := q.KeySet[data.Key]; ok {
 		return ErrKeyExist
 	}
