0a8a302d22d8488d285299e5460ba23b78446a0a
Pedram Sadeghian <pedy4000@gmail.com>
2024-02-11 16:16:02 +0330

    Revert "change helper structures"
    This reverts commit 6c76d963ee463b6f8cb132feb44392655de6fe7f.

--- internal/helper/helper.go
+++ internal/helper/helper.go
@@ -3,36 +3,24 @@ package helper
 import (
 	"fmt"
 	"io"
-	"net"
 	"net/http"
 
-	"github.com/System-Analysis-and-Design-2023-SUT/Server/internal/settings"
 	models "github.com/System-Analysis-and-Design-2023-SUT/Server/models/queue"
 	"github.com/hashicorp/memberlist"
 )
 
 type Helper struct {
 	list *memberlist.Memberlist
-	st   *settings.Settings
 }
 
 func (h *Helper) Read(data models.Data) error {
-	ips, er := net.LookupIP(h.st.Replica.Hostname[0])
-	if er != nil {
-		fmt.Println("Error:", er)
-	}
-
-	ip, ipnet, err := net.ParseCIDR(h.st.Replica.Subnet)
-	if err != nil {
-		fmt.Println("Error parsing subnet:", err)
-	}
-
-	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
-		if ip.Equal(ips[0]) {
+	for _, m := range h.list.Members() {
+		if m == h.list.LocalNode() {
 			continue
 		}
 
-		response, err := http.Get(fmt.Sprintf("http://%s:8080/_pull?key=%s", ip, data.Key))
+		var address = string(m.Meta)
+		response, err := http.Get(fmt.Sprintf("http://%s:8080/_pull?key=%s", address, data.Key))
 		if err != nil {
 			continue
 		}
@@ -43,60 +31,41 @@ func (h *Helper) Read(data models.Data) error {
 }
 
 func (h *Helper) Write(data models.Data) error {
-	ips, er := net.LookupIP(h.st.Replica.Hostname[0])
-	if er != nil {
-		fmt.Println("Error:", er)
-	}
-
-	ip, ipnet, err := net.ParseCIDR(h.st.Replica.Subnet)
-	if err != nil {
-		fmt.Println("Error parsing subnet:", err)
-	}
-
-	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
-		if ip.Equal(ips[0]) {
+	for _, m := range h.list.Members() {
+		if m == h.list.LocalNode() {
+			continue
+		}
+		if len(m.Name) > 6 && m.Name[:len(m.Name)-4] == "sad-server-1" {
 			continue
 		}
 
-		first := false
-		for _, m := range h.list.Members() {
-			if string(m.Meta) == ip.String() && len(m.Name) > 6 && m.Name[:len(m.Name)-4] == "sad-server-1" {
-				first = true
-			}
-		}
-		if first {
-			continue
-		}
-
-		response, _ := http.Post(fmt.Sprintf("http://%s:8080/_push?key=%s&value=%s", ip, data.Key, data.Value),
+		var address = string(m.Meta)
+		response, err := http.Post(fmt.Sprintf("http://%s:8080/_push?key=%s&value=%s", address, data.Key, data.Value),
 			"application/json",
 			nil,
 		)
+		if err != nil {
+			continue
+		}
 
 		defer response.Body.Close()
 	}
-
-	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
-		if ip.Equal(ips[0]) {
+	for _, m := range h.list.Members() {
+		if m == h.list.LocalNode() {
 			continue
 		}
-
-		first := false
-		for _, m := range h.list.Members() {
-			if string(m.Meta) == ip.String() && len(m.Name) > 6 && m.Name[:len(m.Name)-4] == "sad-server-1" {
-				first = true
+		if len(m.Name) > 6 && m.Name[:len(m.Name)-4] == "sad-server-1" {
+			var address = string(m.Meta)
+			response, err := http.Post(fmt.Sprintf("http://%s:8080/_push?key=%s&value=%s", address, data.Key, data.Value),
+				"application/json",
+				nil,
+			)
+			if err != nil {
+				continue
 			}
-		}
-		if !first {
-			continue
-		}
 
-		response, _ := http.Post(fmt.Sprintf("http://%s:8080/_push?key=%s&value=%s", ip, data.Key, data.Value),
-			"application/json",
-			nil,
-		)
-
-		defer response.Body.Close()
+			defer response.Body.Close()
+		}
 	}
 	return nil
 }
@@ -125,15 +94,6 @@ func (h *Helper) GetQueue() ([]byte, error) {
 	return nil, ErrQueueNotFound
 }
 
-func inc(ip net.IP) {
-	for j := len(ip) - 1; j >= 0; j-- {
-		ip[j]++
-		if ip[j] > 0 {
-			break
-		}
-	}
-}
-
 func (h *Helper) GetFirst() string {
 	for _, m := range h.list.Members() {
 		if len(m.Name) > 6 && m.Name[:len(m.Name)-4] == "sad-server-1" {
@@ -144,13 +104,12 @@ func (h *Helper) GetFirst() string {
 	return ""
 }
 
-func NewHelper(list *memberlist.Memberlist, st *settings.Settings) (*Helper, error) {
+func NewHelper(list *memberlist.Memberlist) (*Helper, error) {
 	if list == nil {
 		return nil, ErrNilMemberlist
 	}
 
 	return &Helper{
 		list: list,
-		st:   st,
 	}, nil
 }
